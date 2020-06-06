ce7505a836c8bf5eb3f59baabc1343e010b4ea54
xiecheng <1519955707@qq.com>
2020-06-06 22:16:50 +0800

    修复了时间计算器分钟和秒钟可以大于60的bug

--- 时间计算器/time_calculator/src/App.vue
+++ 时间计算器/time_calculator/src/App.vue
@@ -1,66 +1,78 @@
 <template>
   <div id="app">
-      <div>
-        <input id="hour" type="text" :value="hour"> 时
-        <input id="minute" type="text" :value="minute"> 分
-        <input id="second" type="text" :value="second"> 秒
-        &nbsp;  <input type="button" style="width:80px" value="+" @click="calculate_time()">
-        &nbsp;  <input type="button" style="width:80px" value="清空" @click="clear_time()">
-        <hr>
-        <div id="total_time">
-          total time: {{hour}}:{{minute}}:{{second}}
-        </div>
-      </div>
-      
+    <div>
+      <input id="hour" type="number" v-model:value="hour"> 时
+      <input id="minute" type="number" v-model:value="minute"> 分
+      <input id="second" type="number" v-model:value="second"> 秒
+      &nbsp;
+      <input type="button" style="width:80px" value="+" @click="calculate_time()">
+      &nbsp;
+      <input type="button" style="width:80px" value="清空" @click="clear_time()">
+      <hr>
+      <div id="total_time">total time: {{total_hour}}:{{total_minute}}:{{total_second}}</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
+  data() {
     return {
-      hour:0,
-      minute:0,
-      second:0
-    }
+      hour: 0,
+      minute: 0,
+      second: 0,
+      total_hour: 0,
+      total_minute: 0,
+      total_second: 0
+    };
   },
-  methods:{
-    calculate_time:function(){
-      let _hour = Number(document.getElementById('hour').value);
-      let _minute = Number(document.getElementById('minute').value);
-      let _second = Number(document.getElementById('second').value);
-
-      if(_second + this.second >= 60){
-        this.second = _second + this.second-60;
-        this.minute += 1
-      }else{
-        this.second = _second + this.second
+  methods: {
+    calculate_time() {
+      if (
+        Number(this.second) < 60 &&
+        this.total_second + Number(this.second) < 60
+      ) {
+        this.total_second += Number(this.second);
+      } else if (Number(this.second) >= 60) {
+        this.second = 0;
+        alert("秒数不能超过60s，请重新输入");
+      } else {
+        this.total_second = this.total_second + Number(this.second) - 60;
+        this.total_minute += 1;
       }
 
-      if(_minute + this.minute >= 60){
-        this.minute = _minute + this.minute-60;
-        this.hour += 1
-      }else{
-        this.minute = _minute + this.minute
+      if (
+        Number(this.minute) < 60 &&
+        this.total_minute + Number(this.minute) < 60
+      ) {
+        this.total_minute += Number(this.minute);
+      } else if (Number(this.minute) >= 60) {
+        this.minute = 0;
+        alert("分钟数不能超过60min，请重新输入");
+      } else {
+        this.total_minute = this.total_minute + Number(this.minute) - 60;
+        this.total_hour += 1;
       }
 
-      this.hour = _hour + this.hour
+      this.total_hour += Number(this.hour);
 
+      this.hour = 0;
+      this.minute = 0;
+      this.second = 0;
     },
 
-  clear_time:function(){
-      this.hour = 0
-      this.minute = 0
-      this.second = 0
-      document.getElementById('hour').value = 0;
-      document.getElementById('minute').value = 0;
-      document.getElementById('second').value = 0;
+    clear_time: function() {
+      this.hour = 0;
+      this.minute = 0;
+      this.second = 0;
+      this.total_hour = 0;
+      this.total_minute = 0;
+      this.total_second = 0;
     }
   }
-}
+};
 </script>
 
 
 <style>
-
 </style>
